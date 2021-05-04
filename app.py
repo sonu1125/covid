f5f9f39267de38cc912e57d831ad2a29aa33f925
yashwanth125 <b.yashwanth.reddy@intel.com>
2021-05-04 08:39:49 +0530

    added5

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ cf_port = os.getenv("PORT")
 @app.route("/", methods=["GET", "POST"])
 def home():
 	response =requests.get("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=601201&date=10-05-2021")
-	return response.content
+	return (response.content)
 
 if __name__ == "__main__":
 	if cf_port is None:
