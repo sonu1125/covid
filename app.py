e9df3f7e677961d3b1af55850b2f95603c6cff8c
yashwanth125 <b.yashwanth.reddy@intel.com>
2021-05-04 09:35:43 +0530

    added7

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ cf_port = os.getenv("PORT")
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-	#headers={"x-api-key":"3sjOr2rmM52GzhpMHjDEE1kpQeRxwFDr4YcBEimi"}
-	response =requests.post("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=601201&date=10-05-2021")
+	headers={"x-api-key":"3sjOr2rmM52GzhpMHjDEE1kpQeRxwFDr4YcBEimi"}
+	response =requests.get("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=601201&date=10-05-2021",headers=headers)
 	return (str(response.content))
 
 if __name__ == "__main__":
