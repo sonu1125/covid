5e7d917f6c70814c823098633b405aa32a5f75b7
yashwanth125 <b.yashwanth.reddy@intel.com>
2021-05-04 10:36:48 +0530

    added9

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ cf_port = os.getenv("PORT")
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-	headers={ "x-api-key": "3sjOr2rmM52GzhpMHjDEE1kpQeRxwFDr4YcBEimi" }
-	response =requests.get("https://api.demo.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=601201&date=03-05-2021",headers=headers)
+	headers={ "X-API-KEY": "3sjOr2rmM52GzhpMHjDEE1kpQeRxwFDr4YcBEimi" }
+	response =requests.get("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=601201&date=03-05-2021",header=headers)
 	return (str(response.content))
 
 if __name__ == "__main__":
--- t.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=601201&date=03-05-2021"
-header={"X-API-KEY" : 'qwertyuiop'}
-
-response =requests.get(url)
-
-print(response.content)
