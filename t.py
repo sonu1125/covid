e6874e479afe249f53cf6974e5deba8664ee7378
yashwanth125 <b.yashwanth.reddy@intel.com>
2021-05-04 10:29:45 +0530

    added9

--- t.py
+++ t.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "https://api.demo.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=601201&date=03-05-2021"
+url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=601201&date=03-05-2021"
 header={"X-API-KEY" : 'qwertyuiop'}
 
 response =requests.get(url, params=header)
