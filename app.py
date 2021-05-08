594136e2a3bc29ff2d34f9f158b3a233ae28e6ff
yashwanth125 <b.yashwanth.reddy@intel.com>
2021-05-08 14:32:47 +0530

    added hosp link2

--- app.py
+++ app.py
@@ -9,10 +9,34 @@ headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleW
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-	response =requests.get("https://api.covidbedsindia.in/v1/storages/608982f703eef3de2bd05a72/Bengaluru",verify=False)
+	response =requests.get("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=601201&date=03-05-2021",verify=False,headers=headers)
 	return (str(response.content))
 
+@app.route("/blr", methods=["GET", "POST"])
+def blr():
+	response =requests.get("https://api.covidbedsindia.in/v1/storages/608982f703eef3de2bd05a72/Bengaluru",verify=False)
+	return (response.content)
 
+
+
+@app.route("/chn", methods=["GET", "POST"])
+def chn():
+	response =requests.get("https://api.covidbedsindia.in/v1/storages/6089820e03eef3b588d05a6d/Tamil%20Nadu",verify=False)
+	return (response.content)
+
+
+
+@app.route("/ap", methods=["GET", "POST"])
+def ap():
+	response =requests.get("https://api.covidbedsindia.in/v1/storages/608982e003eef31f34d05a71/Andhra%20Pradesh",verify=False)
+	return (response.content)
+
+
+@app.route("/hyd", methods=["GET", "POST"])
+def hyd():
+	response =requests.get("https://api.covidbedsindia.in/v1/storages/6089829403eef36d93d05a6f/Telangana",verify=False)
+	return (response.content)
+s
 @app.route('/test2', methods=["GET", "POST"]) 
 def test2():
 	PINCODE = request.args.get('answer')
